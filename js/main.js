05cc711538d359d0737a1e8357b2c3c254a94c5f
momashaly <devmashaly@gmail.com>
2021-03-27 01:08:25 +0200

    add project file

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// optional
+		$('#blogCarousel').carousel({
+				interval: 500
+        });
+
+
+
+  
+
+  $(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 800) {
+          $('#back-to-top').fadeIn();
+        } else {
+          $('#back-to-top').fadeOut();
+        }
+      });
+      // scroll body to 0px on click
+      $('#back-to-top').click(function () {
+        $('body,html').animate({
+          scrollTop: 0
+        }, 400);
+        return false;
+      });
+  });
