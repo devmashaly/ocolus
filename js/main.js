fc1ff8f5c9275b5f925b7053a0c8ac05b2174644
momashaly <devmashaly@gmail.com>
2021-03-31 21:55:35 +0200

    add shop page

--- js/main.js
+++ js/main.js
@@ -23,3 +23,60 @@
         return false;
       });
   });
+
+  $('.first').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:2,
+            nav:false,
+        },
+        600:{
+            items:3,
+            nav:false,
+        },
+        1000:{
+            items:4,
+            
+        }
+    }
+})
+
+$('.Column-list').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  dots:false,
+  responsive:{
+      0:{
+          items:2,
+          nav:false,
+      },
+      600:{
+          items:3,
+      },
+      1000:{
+          items:5,     
+      }
+  }
+})
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+
+
+
+  
+
+           
